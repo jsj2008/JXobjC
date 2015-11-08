a1d4041453ffa0f7c4383c26e1a417ace3520b1f
David Mackay <davidjx8p@gmail.com>
2015-11-08 14:00:17 +0000

    AUTOMGR: More work on the Automgr Tracing GC.

--- objcrt/automgr.m
+++ objcrt/automgr.m
@@ -17,9 +17,9 @@ typedef struct _zoneTblEnt
     void * start;
     size_t length;
     /* If it is root, then it is never un-marked.
-     * If it is copy, then it is copied for new threads.
-     * If it is object, we call its ARC_dealloc routine. */
-    BOOL marked, root, copy, object;
+    * If it is copy, then it is copied for new threads.
+    * If it is object, we call its ARC_dealloc routine. */
+    BOOL marked, traced, root, copy, object;
 
     struct _zoneTblEnt * next;
 } zoneTblEnt;
@@ -31,6 +31,7 @@ typedef struct _Automgr
     zoneTblEnt * zoneTbl;
 } Automgr;
 
+static size_t wordSize = sizeof (uintptr_t);
 static pthread_key_t mgrForThread;
 static pthread_mutexattr_t recursiveAttr;
 
@@ -85,6 +86,7 @@ void AMGR_add_zone (void * start, size_t length, BOOL isRoot, BOOL isCopy,
     newEntry->copy   = isCopy;
     newEntry->object = isObject;
     newEntry->marked = NO;
+    newEntry->traced = NO;
 
     newEntry->next = mgr->zoneTbl;
     mgr->zoneTbl   = newEntry;
@@ -106,11 +108,60 @@ void AMGR_remove_zone (void * location)
     }
 }
 
+zoneTblEnt * AMGR_find_zone (void * location)
+{
+    zoneTblEnt ** it;
+
+    for (it = &((Automgr *)ThrdMgr ())->zoneTbl; *it; it = &(*it)->next)
+    {
+        if ((*it)->start == location)
+            return *it;
+    }
+    return 0;
+}
+
+/* CStdLib equivalents */
+
+void * AMGR_alloc (size_t bytes)
+{
+    void * ptr = malloc (bytes);
+
+    AMGR_add_zone (ptr, bytes, NO, NO, NO);
+
+    return ptr;
+}
+
+void * AMGR_calloc (size_t num, size_t bytes)
+{
+    void * ptr = calloc (num, bytes);
+
+    AMGR_add_zone (ptr, num * bytes, NO, NO, NO);
+
+    return ptr;
+}
+
+void * AMGR_realloc (void * location, size_t newBytes)
+{
+    zoneTblEnt * zone = AMGR_find_zone (location);
+    void * newPtr     = realloc (location, newBytes);
+
+    zone->start  = newPtr;
+    zone->length = newBytes;
+
+    return newPtr;
+}
+
+void AMGR_free (void * location)
+{
+    AMGR_remove_zone (location);
+    free (location);
+}
+
 /* This method will, if `location' matches the address of a zone,
  * mark it.
  * Should this check if location falls within the range of a zone, or
  * just equality? */
-void AMGR_ptr_for_address (uintptr_t location)
+zoneTblEnt * AMGR_ptr_for_address (uintptr_t location)
 {
     zoneTblEnt ** it;
 
@@ -119,8 +170,10 @@ void AMGR_ptr_for_address (uintptr_t location)
         if ((*it)->start == (void *)location)
         {
             (*it)->marked = YES;
+            return *it;
         }
     }
+    return 0;
 }
 
 void AMGR_unmark_all_zones ()
@@ -128,18 +181,40 @@ void AMGR_unmark_all_zones ()
     zoneTblEnt ** it;
 
     for (it = &((Automgr *)ThrdMgr ())->zoneTbl; *it != 0; it = &(*it)->next)
-        (*it)->marked = NO;
+        ((*it)->marked = NO, (*it)->traced = NO);
 }
 
 /* Tracing */
 
+void AMGR_trace_zone (zoneTblEnt * zone)
+{
+    uintptr_t * curPtr;
+
+    if (zone->traced)
+        return;
+    else
+        zone->traced = YES;
+
+    for (curPtr = (uintptr_t *)zone->start;
+         curPtr <= (uintptr_t *)zone->start + zone->length;
+         curPtr = (uintptr_t *)(((char *)curPtr) + wordSize))
+    {
+        zoneTblEnt * found;
+
+        if (*curPtr % wordSize)
+            continue;
+        else if ((found = AMGR_ptr_for_address (*curPtr)))
+            AMGR_trace_zone (found);
+    }
+}
+
 void AMGR_trace_terminal_stack_extent ()
 {
     Automgr * mgr = ThrdMgr ();
+    zoneTblEnt * found;
     uintptr_t *final, *curPtr;
-    size_t wordSize = sizeof (uintptr_t);
-    void * top      = 0;
-    final           = (uintptr_t *)&top;
+    void * top = 0;
+    final      = (uintptr_t *)&top;
 
     if (mgr->stkBegin > (void *)final)
         for (curPtr = final; curPtr <= (uintptr_t *)mgr->stkBegin;
@@ -147,7 +222,8 @@ void AMGR_trace_terminal_stack_extent ()
         {
             if (*curPtr % wordSize)
                 continue;
-            AMGR_ptr_for_address (*curPtr);
+            else if ((found = AMGR_ptr_for_address (*curPtr)))
+                AMGR_trace_zone (found);
         }
     else
         for (curPtr = final; curPtr >= (uintptr_t *)mgr->stkBegin;
@@ -155,7 +231,8 @@ void AMGR_trace_terminal_stack_extent ()
         {
             if (*curPtr % wordSize)
                 continue;
-            AMGR_ptr_for_address (*curPtr);
+            else if ((found = AMGR_ptr_for_address (*curPtr)))
+                AMGR_trace_zone (found);
         }
 }
 
@@ -170,7 +247,20 @@ void AMGR_trace_stack_extent ()
     current_extent ();
 }
 
-void AMGR_trace () { AMGR_trace_stack_extent (); }
+void AMGR_trace_roots ()
+{
+    zoneTblEnt ** it;
+    for (it = &((Automgr *)ThrdMgr ())->zoneTbl; *it != 0; it = &(*it)->next)
+        if ((*it)->root)
+            AMGR_trace_zone (*it);
+}
+
+void AMGR_trace ()
+{
+    AMGR_unmark_all_zones ();
+    AMGR_trace_stack_extent ();
+    AMGR_trace_roots ();
+}
 
 void AMGR_sweep () {}
 
