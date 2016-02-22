ce799112e49efdc367c9f981650cbdde9712f71f
David Mackay <davidjx8p@gmail.com>
2016-02-22 23:09:05 +0000

    OBJECT KIT: Make VolatileReference really volatile.

--- objpak/VolatileReference.m
+++ objpak/VolatileReference.m
@@ -1,29 +1,49 @@
 /* Copyright (c) 2015 D. Mackay. All rights reserved. */
 
-#include "gc.h"
+#include <gc.h>
+#import "objcrt.h"
+#import "Exceptn.h"
 #import "VolatileReference.h"
 
 @implementation VolatileReference
 
+- (VolatileReference<T> *)newWithReference:(volatile T)ref
+{
+    return [[self alloc] initWithReference:ref];
+}
+
 - (VolatileReference<T> *)initWithReference:(volatile T)ref
 {
+    if (reference)
+        [Exception signal:"VolatileReferences may not be initialised twice."];
     [super init];
-    GC_general_register_disappearing_link ((void *)&reference, ref);
-    reference = ref + 1;
+    reference = OC_MallocAtomic (sizeof (id) * 2);
+    GC_general_register_disappearing_link ((void *)reference, ref);
+    reference[0] = ref;
+    reference[1] = ref;
     return self;
 }
 
-- (uintptr_t)hash { return [reference - 1 hash]; }
+- (uintptr_t)hash { return [reference[0] hash]; }
 
 - (BOOL)isEqual:anObject
 {
     if ([anObject isKindOf:VolatileReference])
-        return [reference - 1 isEqual:anObject.reference];
+    {
+        if (reference[0] && anObject.reference)
+            return [reference[0] isEqual:anObject.reference];
+        else
+            return reference[1] == anObject.originalReference;
+    }
     else
-        return [anObject isEqual:reference - 1];
+        return [anObject isEqual:reference[0]];
 }
 
-- (T)reference { return reference - 1; }
+- (char *)str { return [reference[0] str]; }
+
+- (T)reference { return reference[0]; }
+
+- (T)originalReference { return reference[1]; }
 
 - (BOOL)isValid { return reference ? YES : NO; }
 
--- objpak/hdr/VolatileReference.h
+++ objpak/hdr/VolatileReference.h
@@ -15,13 +15,18 @@
  */
 @interface VolatileReference <T> : Object
 {
-    volatile id reference;
+    volatile id *reference;
 }
 
+/* Creates a new VolatileReference with the specified object reference. */
+- (VolatileReference<T> *)newWithReference:(volatile T)ref;
 /*! Initialises a new VolatileReference with the specified object reference. */
 - (VolatileReference<T> *)initWithReference:(volatile T)ref;
-/*! Retrieves the object reference hidden inside. */
+/*! Retrieves the object reference hidden inside. If the object has since been
+ * deallocates, returns nil. */
 - (T)reference;
+/*! Retrieves the original value of object reference hidden inside. */
+- (T)originalReference;
 /*! Answers whether the referenced object is valid and still exists. */
 - (BOOL)isValid;
 
